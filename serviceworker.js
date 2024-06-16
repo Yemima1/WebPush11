4ebc7aa671ed07bcd932d87340f0adae44bc66d1
Yemima1 <jyemimaf@gmail.com>
2024-06-16 05:42:23 +0530

    Update serviceworker.js

--- serviceworker.js
+++ serviceworker.js
@@ -1,3 +1,3 @@
 /*! For license information please see serviceworker.js.LICENSE.txt */
 importScripts("https://cdn.moengage.com/release/dc_1/serviceworker_cdn.min.latest.js");
-importScripts('https://cdn.moengage.com/webpush/releases/serviceworker_cdn.min.latest.js');
+importScripts("https://cdn.moengage.com/webpush/releases/serviceworker_cdn.min.latest.js");
